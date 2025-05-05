3aa2c006c3ed3e875684a64ef85bc9acec9004a3
novlan1 <1576271227@qq.com>
2025-05-06 00:11:34 +0800

    chore: add extra:install

--- script/extra-install.sh
+++ script/extra-install.sh
@@ -1,8 +1,9 @@
 pnpm i @plugin-light/project-config-vite@latest @plugin-light/project-config-uni-vite@latest @plugin-light/project-config-vue@latest \
 @plugin-light/project-config-uni-vue@latest @plugin-light/shared@latest @plugin-light/shared-vue2@latest \
 @plugin-light/uni-read-pages-vite@latest @plugin-light/import-meta-resolve@latest @plugin-light/preprocess@latest \
-eslint-config-light@latest eslint-config-light-vue3@latest eslint-plugin-light@latest \
-net-cli@latest  -D 
+eslint-config-light@latest eslint-config-light-vue3@latest eslint-plugin-light@latest eslint-config-rookie@latest \
+net-cli@latest  -D
 
-pnpm i t-comm@latest press-ui@latest press-plus@latest press-next@latest \
+pnpm i t-comm@latest press-ui@latest press-plus@latest press-next@latest press-ui-next@latest swiper-next@latest \
+press-tdesign-vue-next@latest press-element-plus@latest press-shared@latest press-hooks@latest \
 ebus-light@latest share-light@latest vconsole-helper@latest
