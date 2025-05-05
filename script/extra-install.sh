0470377a12761528808b85af51daeaf1cd4b1cde
novlan1 <1576271227@qq.com>
2025-05-05 23:59:48 +0800

    chore: add extra:install

---
+++ script/extra-install.sh
@@ -0,0 +1,8 @@
+pnpm i @plugin-light/project-config-vite@latest @plugin-light/project-config-uni-vite@latest @plugin-light/project-config-vue@latest \
+@plugin-light/project-config-uni-vue@latest @plugin-light/shared@latest @plugin-light/shared-vue2@latest \
+@plugin-light/uni-read-pages-vite@latest @plugin-light/import-meta-resolve@latest @plugin-light/preprocess@latest \
+eslint-config-light@latest eslint-config-light-vue3@latest eslint-plugin-light@latest \
+net-cli@latest  -D 
+
+pnpm i t-comm@latest press-ui@latest press-plus@latest press-next@latest \
+ebus-light@latest share-light@latest vconsole-helper@latest
