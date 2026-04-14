13f56a1be8b728c31ad946aa46e7d19992b9d197
novlan1 <1576271227@qq.com>
2026-04-14 15:18:33 +0800

    chore: update deps

--- script/extra-install.sh
+++ script/extra-install.sh
@@ -1,11 +1,38 @@
-pnpm i @plugin-light/project-config-vite@latest @plugin-light/project-config-uni-vite@latest @plugin-light/project-config-vue@latest \
-@plugin-light/project-config-uni-vue@latest @plugin-light/shared@latest @plugin-light/shared-vue2@latest \
-@plugin-light/uni-read-pages-vite@latest @plugin-light/import-meta-resolve@latest @plugin-light/preprocess@latest \
-eslint-config-light@latest eslint-config-light-vue3@latest eslint-plugin-light@latest eslint-config-rookie@latest \
-net-cli@latest  stylelint-config-common@latest -D
+pnpm i \
+@plugin-light/project-config-vite@latest \
+@plugin-light/project-config-uni-vite@latest \
+@plugin-light/project-config-vue@latest \
+@plugin-light/project-config-uni-vue@latest \
+@plugin-light/shared@latest \
+@plugin-light/shared-vue2@latest \
+@plugin-light/uni-read-pages-vite@latest \
+@plugin-light/import-meta-resolve@latest \
+@plugin-light/preprocess@latest \
+eslint-config-light@latest \
+eslint-config-light-vue3@latest \
+eslint-plugin-light@latest \
+eslint-config-rookie@latest \
+net-cli@latest \
+stylelint-config-common@latest \
+-D
 
-pnpm i t-comm@latest press-ui@latest press-plus@latest press-next@latest press-ui-next@latest swiper-next@latest \
-press-tdesign-vue-next@latest press-element-plus@latest press-shared@latest press-hooks@latest \
-ebus-light@latest share-light@latest vconsole-helper@latest \
-press-swiper@latest mime-light@latest vue-next-lazyload@latest \
-tdesign-uniapp@latest
+pnpm i \
+t-comm@latest \
+press-ui@latest \
+press-plus@latest \
+press-next@latest \
+press-ui-next@latest \
+swiper-next@latest \
+press-tdesign-vue-next@latest \
+press-element-plus@latest \
+press-shared@latest \
+press-hooks@latest \
+ebus-light@latest \
+share-light@latest \
+vconsole-helper@latest \
+press-swiper@latest \
+mime-light@latest \
+vue-next-lazyload@latest \
+tdesign-uniapp@latest \
+@tdesign/uniapp@latest \
+@tdesign/uniapp-chat@latest
