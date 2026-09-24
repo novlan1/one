9842b31e92ed2c19e4568f75572dc7c6d6835c0a
novlan1 <1576271227@qq.com>
2026-09-24 23:16:30 +0800

    feat: use vue-next-lazyload

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { createApp } from 'vue';
 
 import { createPinia } from 'pinia';
 
+// ImageGrid 内部用 v-lazy 渲染图片，需要全局注册 lazy 指令，
+// 否则指令解析不到，img 不会被赋值 src，页面上全是空图（只剩 alt 占位）。
+import VueLazyload from 'vue-next-lazyload';
+
 import App from './App.vue';
 import router from './router';
 
@@ -16,5 +20,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueLazyload, {});
 
 app.mount('#app');
